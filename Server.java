3cb88c1cfacc19221d49a002db17b83ca350f355
gizemhayuk <46076160+gizemhayuk@users.noreply.github.com>
2019-04-07 17:43:13 +0300

    Update Server.java

--- Server.java
+++ Server.java
@@ -9,15 +9,27 @@ import java.net.Socket;
 public class Server extends Thread implements Runnable {
 	static Server newest;
 	Socket socket;
+	static String clientChoice;
 	static BufferedReader inFromClient;
 	static PrintWriter serverPrintOut;
 	static int connectionnumber=0;
+	CollectFourDatabase database = new CollectFourDatabase();
 	
 	
 	public Server(Socket s) {
 		socket = s;
 		try {
 			inFromClient = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			clientChoice=inFromClient.toString();
+			 System.out.println("Message Received: " + clientChoice);
+			 if(clientChoice.equals("1")) {
+					database.RegisterData();
+				}
+				else if(clientChoice.equals("2")) {
+					database.Login();
+				}
+			 
+		
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -26,12 +38,13 @@ public class Server extends Thread implements Runnable {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	
+		
 	}
 
 	public static void main(String args[]) throws Exception {
 		ServerSocket ss = new ServerSocket(3333);	
-		CollectFourDatabase database = new CollectFourDatabase();
-		database.RegisterData();
+	
 
 		for (connectionnumber = 0; connectionnumber < 1000; connectionnumber++) {
 			Socket s = null;
@@ -53,5 +66,4 @@ public class Server extends Thread implements Runnable {
 	public void run() {
 		System.out.println("connection was a success.");
 	}
-
 }
