c1e2d99e2219a99088e164bb03e905307ea40683
gizemhayuk <46076160+gizemhayuk@users.noreply.github.com>
2019-04-13 14:24:13 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -8,13 +8,15 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class Client implements Runnable  {
+public class Client implements Runnable {
 
 	static Socket clientSocket = null; // The client socket
 	static PrintWriter os = null; // The output stream
 	static BufferedReader inFromServer = null; // The input stream
 	static Scanner input = new Scanner(System.in); // to get connection IP from user
-	
+	static String serverConfirm=null; 
+	String username, password; // client name and password for login & register
+
 	public static void main(String[] args) {
 
 		int portNumber = 3333;
@@ -43,21 +45,42 @@ public class Client implements Runnable  {
 
 	@Override
 	public void run() {
-		
-		System.out.println("Welcome please type in 1 to login or 2 to register: ");
-		int choice=input.nextInt();
+
+		System.out.println("***HELLO***");
+		System.out.println("Please type in 1 to LOGIN or 2 to REGISTER: ");
+		int choice = input.nextInt();
 		input.nextLine();
 		
-		if(choice==1){
+		//Request for login
+		if (choice == 1) 
+		{
 
-			System.out.println("please enter your username: ");
-			String username=input.nextLine();
-			System.out.println("please enter your password: ");
-			String password=input.nextLine();
+			System.out.println("Please enter your username: ");
+			username = input.nextLine();
+			System.out.println("Please enter your password: ");
+			password = input.nextLine();
+			os.println(choice);
+			os.println(username);
+			os.println(password);
+			
+		} 
+		//Request for registration
+		else if (choice == 2) 
+			{
+			System.out.println("Please create a user name: ");
+			username = input.nextLine();
+			System.out.println("Please create a password: ");
+			password = input.nextLine();
 			os.println(choice);
 			os.println(username);
 			os.println(password);
 		}
 		
-	}//end of run
-}//end of class
+		else
+		{
+			os.println(choice);
+		}
+		
+				
+	}// end of run
+}// end of class
