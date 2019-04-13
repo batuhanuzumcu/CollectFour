e19796905d7629d4fc27c949ca560a66ad373cd4
gizemhayuk <46076160+gizemhayuk@users.noreply.github.com>
2019-04-13 14:20:15 +0300

    Update CollectFourDB.java

--- CollectFourDB.java
+++ CollectFourDB.java
@@ -1,10 +1,10 @@
 package Game;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class CollectFourDB {
@@ -16,26 +16,65 @@ public class CollectFourDB {
 	private final int portNumber = 3306;
 	private final String dbName = "collectfour"; // the name of the database file
 
-	public Connection getConnection() throws SQLException {
+	// Registration for Users
+	public String RegisterData(String clientName, String clientPassword) {
 		Connection conn = null;
+		Statement s = null;
 		Properties connectionProps = new Properties();
-		connectionProps.put("user", this.userName);
-		connectionProps.put("password", this.password);
-		conn = DriverManager.getConnection("jdbc:mysql://" + this.serverName + ":" + this.portNumber + "/" + this.dbName, connectionProps);
-		return conn;
+		connectionProps.put("user", clientName);
+		connectionProps.put("password", clientPassword);
+		try {
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/collectfour?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Turkey");
+
+			s = conn.createStatement();
+
+			// DUPLICATE USERNAME CONTROL
+			String usernameControl = "SELECT * FROM users where username='" + clientName + "'";
+			ResultSet rs = s.executeQuery(usernameControl);
+
+			if (rs.next() == true) {
+				System.out.println("USER NAME ALREADY EXISTS.");
+				return "fail";
+
+				// IF THE USER NAME IS UNIQUE
+			} else {
+				arr = "INSERT INTO users " + "(username,password) " + "VALUES ('" + clientName + "','" + clientPassword
+						+ "')";
+
+				s.execute(arr);
+				return "success";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (s != null) {
+				s.close();
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+
 	}
-	
-	public String Login(String username,String password) {
+
+	// LOGIN
+	public String Login(String username, String password) {
 		// Connect to MySQL
 		Connection conn = null;
 		try {
-			conn = this.getConnection();
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/collectfour?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Turkey");
 		} catch (SQLException e) {
 			System.out.println("ERROR: Could not connect to the database");
 			e.printStackTrace();
 		}
 		try {
-			PreparedStatement st = conn.prepareStatement("SELECT * " + "FROM collectfourtable "+ "WHERE username LIKE "+ "'%"+username+"%'"+" AND password LIKE "+"'%"+password+"%'");
+			PreparedStatement st = conn.prepareStatement("SELECT * " + "FROM users " + "WHERE username LIKE " + "'%"
+					+ username + "%'" + " AND password LIKE " + "'%" + password + "%'");
 			// We write a query to get the data from the table.
 			ResultSet rs = st.executeQuery();
 
@@ -44,17 +83,17 @@ public class CollectFourDB {
 				String x = rs.getString("username");
 				arr = x.replace("\n", ",");
 			}
-			if(!arr.equals(username)){
+			if (!arr.equals(username)) {
 				System.out.println("wrong username and/or password entered. Please try again.");
 				return "fail";
 			}
 			return "success";
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			System.out.println("ERROR: Could not connect the table");
 			e.printStackTrace();
 			return ("failed to get a username from table");
 		}
 
 	}
+
 }
